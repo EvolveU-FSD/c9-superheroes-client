ebd199f91a7d05092942f3437e2aa2a33b38ebad
Danielle Barker <caseybarker86@gmail.com>
2022-11-09 19:51:06 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect as useMyEffect, useState } from "react";
+import CurrentWeather from "./CurrentWeather";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState();
+  const getCurrentWeather = async () => {
+    console.log("hello from getCurrentTime");
+    try {
+      let response = await fetch("/weather");
+      let values = await response.json();
+      console.log(values);
+      setCurrentWeather(values);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  useMyEffect(() => {
+    console.log("hello from useEffect");
+    const id = setInterval(() => {
+      console.log("hello from interval");
+      getCurrentWeather();
+    }, 3000);
+
+    return () => clearInterval(id);
+  }, []);
+  const clearCurrentTime = () => setCurrentWeather(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentWeather && <CurrentWeather currentWeather={currentWeather} />}
+      <button onClick={getCurrentWeather}> Get Current Weather </button>
+      <button onClick={clearCurrentTime}> Clear Current Weather</button>
     </div>
   );
 }
