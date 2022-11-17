5146810a3054b378f188efa92eeb2168e48e5f59
Danielle Barker <caseybarker86@gmail.com>
2022-11-17 16:05:55 -0700

    Created the list superhero component (needs backend still)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import SuperheroForm from "./components/SuperheroForm";
+import SuperheroList from "./components/SuperheroList";
 
 function App() {
   return (
     <div className="App">
       <h1>Superheroes</h1>
+      <SuperheroList />
+      <SuperheroForm />
     </div>
   );
 }
