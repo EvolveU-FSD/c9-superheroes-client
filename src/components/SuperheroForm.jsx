24d4daaeb2690dbe2014ee4458c7e8054d32362e
Greg Richardson <greg.nmr@gmail.com>
2022-11-22 11:37:32 -0700

    feat: superhero form using materialui

--- src/components/SuperheroForm.jsx
+++ src/components/SuperheroForm.jsx
@@ -1,38 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 const SuperheroForm = () => {
-  const [name, setName] = useState("");
-  const [alterEgo, setAlterEgo] = useState("");
+  const [name, setName] = useState('');
+  const [alterEgo, setAlterEgo] = useState('');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const superhero = {
       name,
-      alterEgo,
+      alterEgo
     };
-    const response = await fetch("/api/superhero", {
-      method: "POST",
+    const response = await fetch('/api/superhero', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(superhero),
+      body: JSON.stringify(superhero)
     });
     const newSuperhero = await response.json();
     console.log(newSuperhero);
   };
+
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input onChange={(event) => setName(event.target.value)} value={name} />
-      </label>
-      <label>
-        Alter Ego:
-        <input
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          padding: 2,
+          gap: 2
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          onChange={(event) => setName(event.target.value)}
+          value={name}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Alter Ego"
+          variant="outlined"
           onChange={(event) => setAlterEgo(event.target.value)}
           value={alterEgo}
         />
-      </label>
-      <button type="submit">Add Superhero</button>
+        <Button type="submit" variant="contained" size="large">
+          Add Superhero
+        </Button>
+      </Box>
     </form>
   );
 };
